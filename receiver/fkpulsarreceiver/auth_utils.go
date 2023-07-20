5d55a15fb2335defe81137d4fb84604429e304a4
saikat.de <saikat.de@flipkart.com>
2023-07-20 11:07:34 +0530

    fkpulsar receiver authn fix initial commit

---
+++ receiver/fkpulsarreceiver/auth_utils.go
@@ -0,0 +1,22 @@
+package fkpulsarreceiver
+
+import (
+	"encoding/base64"
+	"encoding/json"
+)
+
+func GeneratePrivateKey(clientId, clientSecret, issuerUrl string) (string, error) {
+	props := map[string]string{
+		"type":          "client_credentials",
+		"client_id":     clientId,
+		"client_secret": clientSecret,
+		"issuer_url":    issuerUrl,
+	}
+	jsonStr, err := json.Marshal(props)
+	if err != nil {
+		return "", err
+	}
+	encodedText := base64.StdEncoding.EncodeToString(jsonStr)
+	data := "data:application/json;base64," + encodedText
+	return data, nil
+}
